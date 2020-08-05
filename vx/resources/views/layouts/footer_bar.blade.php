6c5c230730e52d3155d9ff1a0b3215b1fe9f9411
anold chifamba <anoldchifamba@gmail.com>
2020-08-05 10:57:55 +0200

    ui navigation and links successful

---
+++ vx/resources/views/layouts/footer_bar.blade.php
@@ -0,0 +1,5 @@
+<script type='text/javascript' src='js/jquery.js'></script>
+<script type='text/javascript' src='js/swiper.min.js'></script>
+<script type='text/javascript' src='js/masonry.pkgd.min.js'></script>
+<script type='text/javascript' src='js/jquery.collapsible.min.js'></script>
+<script type='text/javascript' src='js/custom.js'></script>
